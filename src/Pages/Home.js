be2a9ac5850352f35d594b17fbf0b4fb96a5e68d
enoshpaul05 <enoshpaul.u@gmail.com>
2024-06-04 19:04:52 +0530

    Added New Components

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Box } from '@mui/material'
+import HeroBanner from '../Components/HeroBanner'
+import SearchExercise from '../Components/SearchExercise'
+import Exercises from '../Components/Exercises'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExercise />
+      <Exercises />
+    </Box>
   )
 }
 
